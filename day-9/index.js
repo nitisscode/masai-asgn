688e751cb04d77404ba83d8ed5d754bb4ce02a3a
nitisscode <nitishkumar50805@gmail.com>
2024-04-02 22:40:48 +0530

    report card assignment

---
+++ day-9/index.js
@@ -0,0 +1,22 @@
+var name="Nitish Kumar";
+var school="Masai School";
+var grade="A";
+var section="B"
+var rollno="09"
+var physics="80"
+var chemistry="83"
+var math="78"
+
+console.log("Student Report Card")
+console.log()
+
+console.log("Name:-",name)
+console.log("School:-",school)
+console.log("Grade:-",grade)
+console.log("Section:-",section)
+console.log("Roll no:-",rollno)
+console.log()
+console.log("Subjects","  ","Full Marks","  ","Marks Obtained")
+console.log("Physics","     ",100,"           ",physics)
+console.log("Chemistry","   ",100,"           ",chemistry)
+console.log("Maths","       ",100,"           ",math)
